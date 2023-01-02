7f177323c3dfdcee0e4ed0cb304bb1b368677c09
+ <+>
2023-01-02 14:43:53 -0700

    removed active storage attempt items and removed some misc code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   
   before_action :authorize
-
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
-  end
   
   private
    
