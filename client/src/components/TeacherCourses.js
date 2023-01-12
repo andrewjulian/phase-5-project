68bf9b568aaea21ca263aebded2bca1c0e5f8f39
+ <+>
2023-01-12 15:40:28 -0700

    classes appear as cards with enroll button that does not yet function

--- client/src/components/TeacherCourses.js
+++ client/src/components/TeacherCourses.js
@@ -10,8 +10,27 @@ const TeacherCourses = ({ addClassroom, classrooms }) => {
   const [subject, setSubject] = useState("");
   const [errors, setErrors] = useState([]);
 
+  function enroll() {
+    console.log("enroll!");
+  }
+
   const displayClassrooms = classrooms.map((classroom, id) => {
-    return <p key={id}>{classroom.name}</p>;
+    return (
+      <div key={id} className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{classroom.name}</div>
+          <p className="text-gray-700 text-base">{classroom.subject}</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <button
+            onClick={enroll}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            Enroll
+          </button>
+        </div>
+      </div>
+    );
   });
 
   function handleSubmit(e) {
@@ -74,7 +93,7 @@ const TeacherCourses = ({ addClassroom, classrooms }) => {
         <br />
         <button type="submit">Create!</button>
       </form>
-      {displayClassrooms}
+      <div className="grid grid-cols-4 gap-4">{displayClassrooms}</div>
     </div>
   );
 };
