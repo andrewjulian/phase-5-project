fda5fdda84f99605c72d700546ffe85bec985795
+ <+>
2023-01-12 15:53:57 -0700

    moved new course into a tile in the grid of classes

--- client/src/components/TeacherCourses.js
+++ client/src/components/TeacherCourses.js
@@ -61,32 +61,39 @@ const TeacherCourses = ({ addClassroom }) => {
       <p>You are a {role}</p>
       <p>This is where the classes you teach can be found!</p>
       <br />
-      <h1>Create new Course</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Class Title</label>
-        <input
-          type="text"
-          value={name}
-          placeholder="Enter Course Name"
-          onChange={(e) => setName(e.target.value)}
-          required
-        ></input>
-        <br />
-        <label>Course</label>
-        <select onChange={(e) => setSubject(e.target.value)} defaultValue="">
-          <option value="" disabled>
-            Choose a Class...
-          </option>
-          <option value="Math">Math</option>
-          <option value="Science">Science</option>
-          <option value="History">History</option>
-          <option value="English">English</option>
-          <option value="Foreign Language">Foreign Language</option>
-        </select>
-        <br />
-        <button type="submit">Create!</button>
-      </form>
-      <div className="grid grid-cols-4 gap-4">{displayClassrooms}</div>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <h1>Create new Course</h1>
+          <form onSubmit={handleSubmit}>
+            <label>Class Title</label>
+            <input
+              type="text"
+              value={name}
+              placeholder="Enter Course Name"
+              onChange={(e) => setName(e.target.value)}
+              required
+            ></input>
+            <br />
+            <label>Course</label>
+            <select
+              onChange={(e) => setSubject(e.target.value)}
+              defaultValue=""
+            >
+              <option value="" disabled>
+                Choose a Class...
+              </option>
+              <option value="Math">Math</option>
+              <option value="Science">Science</option>
+              <option value="History">History</option>
+              <option value="English">English</option>
+              <option value="Foreign Language">Foreign Language</option>
+            </select>
+            <br />
+            <button type="submit">Create!</button>
+          </form>
+        </div>
+        {displayClassrooms}
+      </div>
     </div>
   );
 };
