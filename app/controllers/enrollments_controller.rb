7a25209bbbed2dc40973f25c1b1e451ee72dc002
+ <+>
2023-01-23 15:26:21 -0700

    students can enroll in classrooms

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,19 @@
 class EnrollmentsController < ApplicationController
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    enrollment = user.enrollments.create!(enrollment_params)
+    render json: enrollment, status: :created
+  end
+
+  private
+
+  def render_unprocessable_entity(invalid)
+    render json:{error: invalid.record.errors}, status: :unprocessable_entity
+  end
+
+  def enrollment_params
+    params.permit(:id, :student_id, :classroom_id)
+  end
+
 end
