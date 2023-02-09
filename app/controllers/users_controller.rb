6579567675953468e91ab712f1f962fd7d4550f1
+ <+>
2023-02-09 13:25:40 -0700

    fixed new user logout issue, but courses don't actually populate in coure catalog yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
+    session[:user_id] = user.id
     render json: user, status: :accepted
   end
 
