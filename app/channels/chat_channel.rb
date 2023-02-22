38d59f6a268c889dfe6c5b9dab496b63f79f0ce6
+ <+>
2023-02-22 10:29:19 -0700

    reset before AC attempt

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,12 +1,10 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from params[:room]
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    stop_stream_from params[:room]
   end
 
 end
